9b254923d9ab081fff476852a71d5c7dc763b874
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-08-16 16:07:25 +0800

    add item

--- lib/register.dart
+++ lib/register.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -6,26 +7,41 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // We will link this controller with the textfield
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login"),),
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
       body: Column(
         children: <Widget>[
           TextField(
-            decoration: InputDecoration(
-              hintText: "Username"
-            ),
+            controller: emailController,
+            decoration: InputDecoration(hintText: "Email"),
           ),
           TextField(
-            decoration: InputDecoration(
-                hintText: "Password"
-            ),
+            controller: passwordController,
+            decoration: InputDecoration(hintText: "Password"),
           ),
           FlatButton(
             child: Text("Register"),
-            onPressed: (){
-
+            onPressed: () async {
+              print(emailController.text);
+              print(passwordController.text);
+              FirebaseUser user = (await _auth.createUserWithEmailAndPassword(
+                      email: emailController.text.trim(),
+                      password: passwordController.text))
+                  .user;
+              if (user != null) {
+                print("Success");
+              } else {
+                print("fail");
+              }
             },
           )
         ],
